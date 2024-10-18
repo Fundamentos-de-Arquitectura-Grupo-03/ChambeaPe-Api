8868b8e6ba5e5551b13ce64f2fa692812560c628
Ray <ray_punkeki@hotmail.com>
2024-10-18 12:01:03 -0500

    feat: user dtos added

---
+++ src/main/java/com/digitaldark/ChambeaPe_Api/user/dto/request/UserLoginDTO.java
@@ -0,0 +1,21 @@
+package com.digitaldark.ChambeaPe_Api.user.dto.request;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserLoginDTO {
+    @NotBlank(message = "Email cannot be blank")
+    @Email(message = "Email must be valid")
+    private String email;
+
+    @NotBlank(message = "Password cannot be blank")
+    private String password;
+}
